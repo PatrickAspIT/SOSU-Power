de287d30817957cab0b496f3eca9c2216aaef0f9
Patrick Sean <39323@edu.campusvejle.dk>
2024-05-16 10:34:32 +0200

    added medicine class

---
+++ SOSU-Power-9000.Entities/Medicine.cs
@@ -0,0 +1,90 @@
+﻿namespace SOSU_Power_9000.Entities
+{
+    public class Medicine
+    {
+        #region Fields
+        private int medicineId;
+        private string name;
+        private string unit;
+        private int amount;
+        private bool administered;
+        #endregion
+
+        #region Constructors
+        public Medicine(int id, string name, string unit, int amount, bool administered)
+        {
+            this.medicineId = id;
+            this.name = name;
+            this.unit = unit;
+            this.amount = amount;
+            this.administered = administered;
+        }
+        #endregion
+
+        #region Properties
+        public int MedicineId
+        {
+            get => medicineId;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegative(value);
+                if (medicineId != value)
+                {
+                    medicineId = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNullOrWhiteSpace(value);
+                if (name != value)
+                {
+                    name = value;
+                }
+            }
+        }
+        
+        public string Unit
+        {
+            get => unit;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNullOrWhiteSpace(value);
+                if (unit != value)
+                {
+                    unit = value;
+                }
+            }
+        }
+
+        public int Amount
+        {
+            get => amount;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegative(value);
+                if (amount != value)
+                {
+                    amount = value;
+                }
+            }
+        }
+
+        public bool Administered
+        {
+            get => administered;
+            set
+            {
+                if (administered != value)
+                {
+                    administered = value;
+                }
+            }
+        }
+        #endregion
+    }
+}
