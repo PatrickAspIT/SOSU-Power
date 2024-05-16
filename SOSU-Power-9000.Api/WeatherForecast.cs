d63169d8fe43ea954a9d888571b169a019c0f427
Patrick Sean <39323@edu.campusvejle.dk>
2024-05-16 09:48:45 +0200

    added entities library

---
+++ SOSU-Power-9000.Api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace SOSU_Power_9000.Api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
---
+++ SOSU-Power-9000.Entities/Resident.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSU_Power_9000.Entities
+{
+    public class Resident
+    {
+        #region Fields
+        private int residentId;
+        private string name;
+        private string roomNumber;
+
+        // Maybe add these later
+        //private string List<Diagnosis>;
+        //private string List<Prescription>;
+        private string notes;
+        #endregion
+
+        #region Constructors
+        public Resident(int id, string name, string roomNumber, string notes)
+        {
+            this.residentId = id;
+            this.name = name;
+            this.roomNumber = roomNumber;
+            this.notes = notes;
+        }
+        #endregion
+
+        #region Properties
+        public int ResidentId
+        {
+            get => residentId;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegative(value);
+                if (residentId != value)
+                {
+                    residentId = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string RoomNumber
+        {
+            get => roomNumber;
+            set => roomNumber = value;
+        }
+
+        public string Notes
+        {
+            get => notes;
+            set => notes = value;
+        }
+        #endregion
+
+
+    }
+}
