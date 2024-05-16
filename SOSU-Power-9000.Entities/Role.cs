8a7660c121051e0ee0b09e3205c411f9fce15ba8
Patrick Sean <39323@edu.campusvejle.dk>
2024-05-16 10:27:50 +0200

    created role class

---
+++ SOSU-Power-9000.Entities/Role.cs
@@ -0,0 +1,46 @@
+﻿namespace SOSU_Power_9000.Entities
+{
+    public class Role
+    {
+        #region Fields
+        private int roleId;
+        private string roleName;
+        #endregion
+
+        #region Constructors
+        public Role(int id, string roleName)
+        {
+            this.roleId = id;
+            this.roleName = roleName;
+        }
+        #endregion
+
+        #region Properties
+        public int RoleId
+        {
+            get => roleId;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegative(value);
+                if (roleId != value)
+                {
+                    roleId = value;
+                }
+            }
+        }
+
+        public string RoleName
+        {
+            get => roleName;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNullOrWhiteSpace(value);
+                if (roleName != value)
+                {
+                    roleName = value;
+                }
+            }
+        }
+        #endregion
+    }
+}
