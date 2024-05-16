bb9232a29ee1d20312c46922f981c7bed6877d62
Patrick Sean <39323@edu.campusvejle.dk>
2024-05-16 10:31:23 +0200

    created employee class and updated ER Class Diagram

---
+++ SOSU-Power-9000.Entities/Employee.cs
@@ -0,0 +1,91 @@
+﻿namespace SOSU_Power_9000.Entities
+{
+    public class Employee
+    {
+        #region Fields
+        private int employeeId;
+        private List<Task> tasks;
+        private List<Role> roles;
+        private string name;
+        private CareCenter careCenter;
+        #endregion
+
+        #region Constructors
+        public Employee(int id, List<Task> tasks, List<Role> roles, string name, CareCenter careCenter)
+        {
+            this.employeeId = id;
+            this.tasks = tasks;
+            this.roles = roles;
+            this.name = name;
+            this.careCenter = careCenter;
+        }
+        #endregion
+
+        #region Properties
+        public int EmployeeId
+        {
+            get => employeeId;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegative(value);
+                if (employeeId != value)
+                {
+                    employeeId = value;
+                }
+            }
+        }
+
+        public List<Task> Tasks
+        {
+            get => tasks;
+            set
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                if (tasks != value)
+                {
+                    tasks = value;
+                }
+            }
+        }
+
+        public List<Role> Roles
+        {
+            get => roles;
+            set
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                if (roles != value)
+                {
+                    roles = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNullOrWhiteSpace(value);
+                if (name != value)
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public CareCenter CareCenter
+        {
+            get => careCenter;
+            set
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                if (careCenter != value)
+                {
+                    careCenter = value;
+                }
+            }
+        }
+        #endregion
+    }
+}
