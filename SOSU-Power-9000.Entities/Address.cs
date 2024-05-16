d38f8207a728c2237f9a45dfee6f93b16ce312c4
Patrick Sean <39323@edu.campusvejle.dk>
2024-05-16 10:20:52 +0200

    added address class

---
+++ SOSU-Power-9000.Entities/Address.cs
@@ -0,0 +1,91 @@
+﻿namespace SOSU_Power_9000.Entities
+{
+    public class Address
+    {
+        #region Fields
+        private int addressId;
+        private string street;
+        private string city;
+        private string state;
+        private int zip;
+        #endregion
+
+        #region Constructors
+        public Address(int id, string street, string city, string state, int zip)
+        {
+            this.addressId = id;
+            this.street = street;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+        }
+        #endregion
+
+        #region Properties
+        public int AddressId
+        {
+            get => addressId;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegative(value);
+                if (addressId != value)
+                {
+                    addressId = value;
+                }
+            }
+        }
+
+        public string Street
+        {
+            get => street;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNullOrWhiteSpace(value);
+                if (street != value)
+                {
+                    street = value;
+                }
+            }
+        }
+
+        public string City
+        {
+            get => city;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNullOrWhiteSpace(value);
+                if (city != value)
+                {
+                    city = value;
+                }
+            }
+        }
+
+        public string State
+        {
+            get => state;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNullOrWhiteSpace(value);
+                if (state != value)
+                {
+                    state = value;
+                }
+            }
+        }
+
+        public int Zip
+        {
+            get => zip;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegative(value);
+                if (zip != value)
+                {
+                    zip = value;
+                }
+            }
+        }
+        #endregion
+    }
+}
