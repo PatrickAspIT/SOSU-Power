7a743390fd6e3c09616adb56dbd002dd64cb3562
Patrick Sean <39323@edu.campusvejle.dk>
2024-06-04 12:53:35 +0200

    added to MauiProgram

--- SOSU-Power-9000.CareApp/MauiProgram.cs
+++ SOSU-Power-9000.CareApp/MauiProgram.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.Extensions.Logging;
+using SOSU_Power_9000.CareApp.ViewModels;
+using SOSU_Power_9000.Services;
 
 namespace SOSU_Power_9000.CareApp
 {
     public static class MauiProgram
     {
+        private const string url = "https://localhost:7033/";
+
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -14,6 +18,11 @@ namespace SOSU_Power_9000.CareApp
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+            Uri baseUri = new Uri(url);
+            builder.Services.AddScoped<ISosuService>(x => new SosuService(baseUri));
+            builder.Services.AddSingleton<MainPageViewModel>();
+            builder.Services.AddSingleton<MainPage>();
 
 #if DEBUG
     		builder.Logging.AddDebug();
